2e5cc72f6dd89ea16b47a60fc4cc12c93521ef8a
Ben Hurst <BHurst8@gmail.com>
2018-03-27 17:11:49 -0700

    can add more than 1 project

--- js/app.js
+++ js/app.js
@@ -14,16 +14,16 @@ var UserData = {
   techSkills: {
     languages: '',
     tools: [],
-    opSys: [],
+    opSys: []
   },
-  yourProjects: [{ //might need an object constructor fcn here
-    projTitle: '',
-    date: '',
-    url: '',
-    description: '',
-    languages: '',
-    persContributions: ''
-  }],
+  yourProjects: { //might need an object constructor fcn here
+    projTitle: [],
+    date: [],
+    url: [],
+    description: [],
+    languages: [],
+    persContributions: []
+  },
   yourEd: [{ //might need an object constructor fcn here
     school: '',
     location: '',
@@ -71,20 +71,22 @@ function tabHandler(event) {
 
   if (event.target.id === 'infoTab') {
     allFieldsets[0].style.display = 'block';
-  } else if (event.target.id === 'techTab') {
+  } else if (event.target.id === 'statementTab') {
     allFieldsets[1].style.display = 'block';
-  } else if (event.target.id === 'projTab') {
+  } else if (event.target.id === 'techTab') {
     allFieldsets[2].style.display = 'block';
-  } else if (event.target.id === 'edTab') {
+  } else if (event.target.id === 'projTab') {
     allFieldsets[3].style.display = 'block';
-  } else if (event.target.id === 'expTab') {
+  } else if (event.target.id === 'edTab') {
     allFieldsets[4].style.display = 'block';
+  } else if (event.target.id === 'expTab') {
+    allFieldsets[5].style.display = 'block';
   }
+
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-
 
   var nameInput = event.target.nameText.value;
   var careerInput = event.target.careerText.value; //careerText is id for <input>
@@ -105,6 +107,13 @@ function handleSubmit(event) {
 
 function handleSubmit2(event) {
   event.preventDefault();
+  var statementInput = event.target.statementText.value;
+  UserData.persStatement = statementInput;
+  genAllContent();
+}
+
+function handleSubmit3(event) {
+  event.preventDefault();
 
   var languagesInput = event.target.languagesText.value;
   var toolsInput = event.target.toolsText.value;
@@ -118,36 +127,48 @@ function handleSubmit2(event) {
 
 }
 
-function handleSubmit3(event) {
+function handleSubmit4(event) {
   event.preventDefault();
+
+  // var resume = document.getElementById('resume');
+  // removeAllText(resume);
 
   //var targetedInput = event.target;
   //var projectNameInput = event.target.projectNameText[targetedInput].value;
   var projectNameInput = event.target.projectNameText.value;
+  console.log(projectNameInput);
   var projectDateInput = event.target.projectDateText.value;
   var projectLinkInput = event.target.projectLinkText.value;
   var projectDescriptionInput = event.target.projectDescriptionText.value;
   var languagesUsedInput = event.target.languagesUsedText.value;
   var persContributionsInput = event.target.PersContributionsText.value;
 
-  UserData.yourProjects.projTitle = projectNameInput;
-  UserData.yourProjects.date = projectDateInput;
-  UserData.yourProjects.url = projectLinkInput;
-  UserData.yourProjects.description = projectDescriptionInput;
-  UserData.yourProjects.languages = languagesUsedInput;
-  UserData.yourProjects.persContributions = persContributionsInput;
 
-  // var class = document.getElementsByClassName('projectSTuff')
-  // for (var i = 0; index < array.length; i++) {
-  //   var targeteddeal = event.target.class[i].value
-  //   targeteddeal = '';
+  UserData.yourProjects.projTitle.push(projectNameInput);
+  UserData.yourProjects.date.push(projectDateInput);
+  UserData.yourProjects.url.push(projectLinkInput);
+  UserData.yourProjects.description.push(projectDescriptionInput);
+  UserData.yourProjects.languages.push(languagesUsedInput);
+  UserData.yourProjects.persContributions.push(persContributionsInput);
+
+  var projectSelect = document.getElementsByClassName('projectClass');
+
+  for (var i = 0; i < Object.keys(UserData.yourProjects).length; i++) {
+    // console.log(projectSelect);
+    // console.log(projectSelect[0]);
+    // console.log(projectSelect[0].value);
+    // console.log(projectSelect[i]);
+    projectSelect[i].value = '';
+  }
 
   genAllContent();
 
 }
 
-function handleSubmit4(event) {
+function handleSubmit5(event) {
   event.preventDefault();
+
+
 
   var schoolInput = event.target.schoolText.value;
   var locationInput = event.target.locationText.value;
@@ -164,8 +185,10 @@ function handleSubmit4(event) {
 
 }
 
-function handleSubmit5(event) {
+function handleSubmit6(event) {
   event.preventDefault();
+
+
 
   var companyInput = event.target.companyText.value;
   var positionInput = event.target.positionText.value;
@@ -202,23 +225,26 @@ function removeAllText(element) {
 
 function genAllContent() {
   var resume = document.getElementById('resume');
-  console.log(resume);
   removeAllText(resume);
 
   var formEl = document.getElementById('persInfoForm');
   formEl.addEventListener('submit', handleSubmit);
 
-  var formEl2 = document.getElementById('techSkillsForm');
+  var formEl2 = document.getElementById('statementForm');
+  console.log(formEl2);
   formEl2.addEventListener('submit', handleSubmit2);
 
-  var formEl3 = document.getElementById('projectForm');
+  var formEl3 = document.getElementById('techSkillsForm');
   formEl3.addEventListener('submit', handleSubmit3);
 
-  var formEl4 = document.getElementById('edForm');
+  var formEl4 = document.getElementById('projectForm');
   formEl4.addEventListener('submit', handleSubmit4);
 
-  var formEl5 = document.getElementById('expForm');
+  var formEl5 = document.getElementById('edForm');
   formEl5.addEventListener('submit', handleSubmit5);
+
+  var formEl6 = document.getElementById('expForm');
+  formEl6.addEventListener('submit', handleSubmit6);
 
 
 
@@ -232,7 +258,7 @@ function genAllContent() {
   var infoLinks = ['linkedIn', 'gitHub', ];
   contentGen('personalInfo', 'h3', infoLinks[0] + ' | ' + infoLinks[1]);
 
-  contentGen('statement', 'h3', 'Lorem Ipsum a bunch of stuff');
+  contentGen('statement', 'p', UserData.persStatement);
 
   contentGen('technicalSkills', 'h3', 'Technical Skills');
   contentGen('technicalSkills', 'p', 'Languages: ' + UserData.techSkills.languages);
@@ -240,19 +266,26 @@ function genAllContent() {
   contentGen('technicalSkills', 'p', 'Operating Systems: ' + UserData.techSkills.opSys);
 
   contentGen('projects', 'h3', 'Projects');
-  contentGen('projects', 'h4', UserData.yourProjects.projTitle + ' | ' + UserData.yourProjects.date + ' | ' + UserData.yourProjects.url);
-  contentGen('projects', 'p', UserData.yourProjects.description);
-  contentGen('projects', 'h5', 'Languages Used: ' + UserData.yourProjects.languages);
-  contentGen('projects', 'h5', UserData.yourProjects.persContributions);
+
+  for (var i = 0; i < UserData.yourProjects.projTitle.length; i++) {
+
+    contentGen('projects', 'h4', UserData.yourProjects.projTitle[i] + ' | ' + UserData.yourProjects.date[i] + ' | ' + UserData.yourProjects.url[i]);
+    contentGen('projects', 'p', UserData.yourProjects.description[i]);
+    contentGen('projects', 'li', 'Languages Used: ' + UserData.yourProjects.languages[i]);
+    contentGen('projects', 'li', UserData.yourProjects.persContributions[i]);
+    contentGen('projects', 'p', '');
+
+
+  }
 
   contentGen('education', 'h3', 'Education');
   contentGen('education', 'h4', UserData.yourEd.school + ' | ' + UserData.yourEd.location);
   contentGen('education', 'p', UserData.yourEd.degree + ' | ' + UserData.yourEd.gradDate);
-  
+
 
   contentGen('experience', 'h3', 'Experience');
   contentGen('experience', 'h4', UserData.yourExp.company + ' , ' + UserData.yourExp.position + ' | ' + UserData.yourExp.startDate + ' - ' + UserData.yourExp.endDate);
-  contentGen('experience', 'h5', UserData.yourExp.action);
+  contentGen('experience', 'li', UserData.yourExp.action);
 
 }
 
